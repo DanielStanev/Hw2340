053af9729cfdb9d9e269de2405c1b2e13f1ddbbb
DanielStanev <Daniel.Stanev@protonmail.com>
2021-09-08 23:16:40 -0500

    Added Hw1

---
+++ Hw1.asm
@@ -0,0 +1,13 @@
+#   Author:         Daniel Stanev
+#   Course:         CS 2340.005
+#   Date:           08 Sep 2021
+#   Assignment:     Pgm1
+#   Language:       MIPS Assembly
+#
+#   Description: Reads in 2 integer inputs from the
+#   console and displays their, sum, differences, 
+#   product, and quotients.
+
+.data 
+
+.text
