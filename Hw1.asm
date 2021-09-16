172ff77585eb877bf47479ee86888fc1e7fd23ef
DanielStanev <Daniel.Stanev@protonmail.com>
2021-09-16 13:28:43 -0500

    Prof made me comment every single line

--- Hw1.asm
+++ Hw1.asm
@@ -1,3 +1,4 @@
+#   Filename:       Hw1.asm
 #   Author:         Daniel Stanev
 #   Course:         CS 2340.005
 #   Date:           08 Sep 2021
@@ -25,130 +26,124 @@
 main:
 
     # Prompt the user for int A
-    li $v0, 4
-    la $a0, promptA
-    syscall
+    li $v0, 4       # Loads 4 into v0
+    la $a0, promptA # Loads "A: " into a0
+    syscall         # Print "A: " to console
     
     # Stores in the input into $t0
-    li $v0, 5
-    syscall
-    move $t0, $v0
+    li $v0, 5       # Loads 5 into v0
+    syscall         # Stores user input into v0
+    move $t0, $v0   # Moves the result from v0 into t1
     
     # Prompt the user for int B
-    li $v0, 4
-    la $a0, promptB
-    syscall
+    li $v0, 4       # Loads 4 into v0
+    la $a0, promptB # Loads "B: " into a0
+    syscall         # Print "B: " to console
     
     # Stores in the input into $t1
-    li $v0, 5
-    syscall
-    move $t1, $v0
+    li $v0, 5       # Loads 5 into v0
+    syscall         # Stores user input into v0
+    move $t1, $v0   # Moves the result from v0 into t1
 
     # outputs A + B
-    # Prints "A + B = "
-    li $v0, 4
-    la $a0, output1
-    syscall
+    li $v0, 4       # Loads 4 into v0
+    la $a0, output1 # Loads "A + B = " into a0
+    syscall         # Print "A + B = " to console
 
     # Prints the answer to A + B
-    li $v0, 1
-    add $a0, $t0, $t1
-    syscall
+    li $v0, 1           # Loads 1 into v0
+    add $a0, $t0, $t1   # Stores t0 + t1 into a0
+    syscall             # Prints the value of a0 to console
 
     jal insertNewline # Calls procedure to insert a newline character
     
     # outputs A - B
-    # Prints "A - B = "
-    li $v0, 4
-    la $a0, output2
-    syscall
+    li $v0, 4       # Loads 4 into v0
+    la $a0, output2 # Loads "A - B = " into a0
+    syscall         # Print "A - B = " to console
 
     # Prints answer to A - B
-    li $v0, 1
-    sub $a0, $t0, $t1
-    syscall
+    li $v0, 1           # Loads 1 into v0
+    sub $a0, $t0, $t1   # Stores t0 - t1 into a0
+    syscall             # Prints the value of a0 to console
 
     jal insertNewline # Calls procedure to insert a newline character
 
     # outputs B - A
-    # Prints "B - A = "
-    li $v0, 4
-    la $a0, output3
-    syscall
+    li $v0, 4       # Loads 4 into v0
+    la $a0, output3 # Loads "B - A = " into a0
+    syscall         # Print "B - A = " to console
 
     # Prints answer to B - A
-    li $v0, 1
-    sub $a0, $t1, $t0
-    syscall
+    li $v0, 1           # Loads 1 into v0
+    sub $a0, $t1, $t0   # Stores t1 - t0 into a0
+    syscall             # Prints the value of a0 to console
 
     jal insertNewline # Calls procedure to insert a newline character
 
     # outputs A * B
-    # Prints "A * B = "
-    li $v0, 4
-    la $a0, output4
-    syscall
+    li $v0, 4       # Loads 4 into v0
+    la $a0, output4 # Loads "A * B = " into a0
+    syscall         # Print "A * B = " to console
 
     # Prints answer to A * B
-    li $v0, 1
-    mul $a0, $t0, $t1
-    syscall
+    li $v0, 1           # Loads 1 into v0
+    mul $a0, $t0, $t1   # Stores t0 * t1 into a0
+    syscall             # Prints the value of a0 to console
 
     jal insertNewline # Calls precedure to insert a newline character
 
     # Outputs A / B
-    # Prints "A / B = "
-    li $v0, 4
-    la $a0, output5
-    syscall
+    li $v0, 4       # Loads 4 into v0
+    la $a0, output5 # Loads "A / B = " into a0
+    syscall         # Print "A / B = " to console
 
     # Prints answer to A / B
-    div $t0, $t1
-    li $v0, 1
-    mflo $a0
-    syscall
+    div $t0, $t1    # Stores t0 / t1 into lo register and t0 % t1 into hi register
+    li $v0, 1       # Loads 1 into v0
+    mflo $a0        # Stores the value of the lo register to a0
+    syscall         # Prints the value of a0 to console
 
     # Prints " Remainder "
-    li $v0, 4
-    la $a0, remainder
-    syscall
+    li $v0, 4           # Loads 4 into v0
+    la $a0, remainder   # Loads " Remainder " into a0
+    syscall             # Print " Remainder " to console
 
     # Prints answer to A % B
-    li $v0, 1
-    mfhi $a0
-    syscall
+    li $v0, 1   # Loads 1 into v0
+    mfhi $a0    # Stores the value of the hi register into a0
+    syscall     # Prints the value of a0 to console
 
     jal insertNewline # Calls procedure to insert a newline character
 
     # Outputs B / A
-    # Prints "B / A = "
-    li $v0, 4
-    la $a0, output6
-    syscall
+    li $v0, 4       # Loads 4 into v0
+    la $a0, output6 # Loads "B / A = " into a0
+    syscall         # Print "B / A = " to console
 
     # Prints out the answer to B / A
-    div $t1, $t0
-    li $v0, 1
-    mflo $a0
-    syscall
+    div $t1, $t0    # Stores t1 / t0 into lo register and t1 % t0 into hi register
+    li $v0, 1       # Loads 1 into v0
+    mflo $a0        # Stores the value of the lo register into a0
+    syscall         # Prints the value of a0 to console
 
     # Prints " Remainder "
-    li $v0, 4
-    la $a0, remainder
-    syscall
+    li $v0, 4           # Loads 4 into v0
+    la $a0, remainder   # Loads " Remainder " into a0
+    syscall             # Print " Remainder " to console
 
     # Prints out B % A
-    li $v0, 1
-    mfhi $a0
-    syscall
+    li $v0, 1   # Loads 1 into v0
+    mfhi $a0    # Stores the value of the hi register into a0
+    syscall     # Prints the value of a0 to console
 
     # Terminates Program by issuing syscall code 10
-    li $v0, 10
-    syscall
+    li $v0, 10  # Loads 10 into v0
+    syscall     # Terminates Program 
 
 # Procedure to print a newline character
 insertNewline:
-    li $v0, 11   # Loads 11 into v0 to signify printing a single character
-    la $a0, '\n' # Loads '\n' into a0
-    syscall      # Prints '\n'
-    jr $ra       # Returns back to where it was called from
+    li $v0, 11      # Loads 11 into v0 to signify printing a single character
+    la $a0, '\n'    # Loads '\n' into a0
+    syscall         # Prints '\n'
+    jr $ra          # Returns back to where it was called from
